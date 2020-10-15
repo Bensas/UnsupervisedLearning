2532fcf6f6344fc3416fddf6cf83a44e6bdef90a
Bensas <juanbensa@gmail.com>
2020-10-15 00:44:41 -0300

    Fixed for loop

--- kohonen.py
+++ kohonen.py
@@ -11,10 +11,11 @@ class Kohonen():
       if dist < min_dist:
         min_dist = dist
         min_dist_neuron = (ix, iy)
-    return min_dist
+    return min_dist_neuron
 
 
   def train(self, data, epochs=1000):
-    for point in data:
-      closest_neuron = self.get_closest_neuron(point)
+    for _ in range(epochs):
+      for point in data:
+        closest_neuron = self.get_closest_neuron(point)
      
