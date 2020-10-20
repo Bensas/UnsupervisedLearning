899e9207d894e55df31f524ffa697307ebd840e3
Yatex <foliver@gmail.com>
2020-10-20 01:42:54 -0300

    anda la red neuronal, la normalizacion tambien pero el calculo con una libreria externa no da bien

--- ojarule.py
+++ ojarule.py
@@ -3,13 +3,16 @@ import numpy as np
 import pandas as pd
 import math
 import time     
+import scipy.linalg as la
 import matplotlib.pyplot as plt
 from numpy import linalg
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import normalize
 
 class red_neuronal_with_oja(): #perceptron simple
-    def __init__(self, no_of_inputs, values, epochs, learning_rate=0.01):
+    def __init__(self, values, epochs, learning_rate=0.01):
         self.values = values
         self.learning_rate = learning_rate
         self.variables = values.shape[1]
@@ -18,12 +21,11 @@ class red_neuronal_with_oja(): #perceptron simple
         self.bias = 0.01
         self.epochs = epochs
            
-    def predict(self, inputs):
-        summation = np.dot(inputs, self.weights) + self.bias
-        if summation >= 0:
-          return 1
-        else:
-          return -1
+    def predict(self, sample):
+        activ = 0
+        for i in range(self.variables):
+            activ += self.weights[i] * sample[i]
+        return activ
 
     def train(self):
         for epoch in range(self.epochs):
@@ -38,26 +40,35 @@ class red_neuronal_with_oja(): #perceptron simple
 
 def ej1_b():
 
-    features = ['Area','GPD','Inflation','Life.expect', 'Military', 'Pop.growth', 'Unemployment']
     europe_csv = pd.read_csv('europe.csv', names=['Country','Area','GPD','Inflation','Life.expect', 'Military', 'Pop.growth', 'Unemployment'])
     europe_csv = europe_csv.drop([0]) #saco la primer fila de nombres ya que no la quiero en la matriz de datos
     countries = europe_csv.loc[:, 'Country'] #separo valores para trabajr mas facil luego, en registros y variables.
     values = europe_csv.loc[1:, ['Area','GPD','Inflation','Life.expect', 'Military', 'Pop.growth', 'Unemployment']].values.astype(np.float)
-    
+
     print("Raw Values: \n")
     print(values)
 
-    values_normalized = StandardScaler().fit_transform(values)
+    non_categoric = np.array(values)
+    means = np.reshape(non_categoric.mean(axis=0), (-1, non_categoric.shape[1]))
+    ones = np.reshape(np.ones(non_categoric.shape[0]), (-1, 1))
+    mean_cero = non_categoric - (ones * means)
+    
+    values_normalized = normalize(mean_cero, axis=0)
+    
+    # METODO PARA NORMALIZAR ALTERNATIVO DUDOSO
+    # scalar = MinMaxScaler(feature_range=(-1,1))
+    # scalar.fit(values)
+    # values_normalized = scalar.transform(values)
 
     print("Normalized Values: \n")
     print(values_normalized)
 
     # Let's check whether the normalized data has a mean of zero and a standard deviation of one.
 
-    print("Mean: \n")
-    print(np.mean(values_normalized))
-    print("Standard deviation: \n")
-    print(np.std(values_normalized))
+    # print("Mean: \n")
+    # print(np.mean(values_normalized))
+    # print("Standard deviation: \n")
+    # print(np.std(values_normalized))
 
     # With PCA
 
@@ -65,18 +76,22 @@ def ej1_b():
 
     start = time.time()
 
-    feat_cols = [features[i] for i in range(values_normalized.shape[1])]
-    normalised_values = pd.DataFrame(values_normalized,columns=feat_cols)
+    # feat_cols = [features[i] for i in range(values_normalized.shape[1])]
+    # normalised_values = pd.DataFrame(values_normalized,columns=feat_cols)
     # print(normalised_values)
-    pca = PCA(n_components=2)
-    principalComponents = pca.fit_transform(values_normalized)
-    principal_Df = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
+
+    eigenvalues, eigenvectors = np.linalg.eig(np.cov(values_normalized.T))
+    print(eigenvalues)
+
+    # pca = PCA(n_components=1)
+    # principalComponents = pca.fit_transform(values_normalized.T)
+    # principal_Df = pd.DataFrame(data = principalComponents, columns = ['principal component 1'])
 
     end = time.time()
 
-    print(principal_Df)
-    print("\n\n")
-    print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
+    # print(principal_Df)
+    # print("\n\n")
+    # print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
 
     # plt.figure()
     # plt.figure(figsize=(10,10))
@@ -99,7 +114,7 @@ def ej1_b():
     print("\n\n\n WITH OJA'S RULE \n\n\n")
 
     epochs = 1000
-    oja = red_neuronal_with_oja(values_normalized[0].size, values_normalized, epochs)
+    oja = red_neuronal_with_oja( values_normalized, epochs)
     start2 = time.time()
     oja.train()
     end2 = time.time()
