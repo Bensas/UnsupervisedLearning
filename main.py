5252ae55be1a3d3632bf00b32360a5d89028e9c1
Bensas <juanbensa@gmail.com>
2020-10-21 19:46:42 -0300

    Added country grouping plot

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 from kohonen import Kohonen
 from hopfield import Hopfield
 
+KOHONEN_DIMENSION = 4
+
 def custom_fmt(spec):
   return 'Countries'
 
@@ -22,22 +24,29 @@ if command == "1a":
   #   print(row)
 
   print("Data loaded and normalized. Training net...")
-  kohonen_net = Kohonen(dimension=2, data=data, learning_rate=0.001)
+  kohonen_net = Kohonen(dimension=KOHONEN_DIMENSION, data=data, learning_rate=0.001)
   kohonen_net.train(data=data)
   # print(kohonen_net.neurons)
 
   print("Net trained, obtaining results:")
   
   neuron_countries = []
-  for i in range(2):
+  for i in range(KOHONEN_DIMENSION):
     row = []
-    for j in range(2):
-      row.append('')
+    for j in range(KOHONEN_DIMENSION):
+      row.append('(' + str(i) + ', ' + str(j) + ')')
     neuron_countries.append(row)
 
   for country_name, country_weights in zip(country_names, data):
     neuron = kohonen_net.get_closest_neuron(country_weights)
     neuron_countries[neuron[1]][neuron[0]] += country_name + '\n'
+  
+  for i in range(len(neuron_countries)):
+    for j in range(len(neuron_countries[0])):
+      plt.text(i, j, neuron_countries[i][j]);
+  plt.xlim(0, len(neuron_countries)*1.2)
+  plt.ylim(0, len(neuron_countries)*1.2)
+  plt.show()
   print(neuron_countries)
   # kohonen_net.plot_average_distances()
   kohonen_net.plot_average_distances_with_countries(neuron_countries=neuron_countries)
@@ -49,4 +58,8 @@ elif command == "2":
   data = raw_data.to_numpy()
   hopfield_net = Hopfield(data)
   hopfield_net.train(data)
+
+  test_patterns, expected_result_indexes = Hopfield.machine_of_patterns(data, 0.2, 50)
+  for index, pattern in enumerate(test_patterns):
+    stabilized_pattern = hop
   
